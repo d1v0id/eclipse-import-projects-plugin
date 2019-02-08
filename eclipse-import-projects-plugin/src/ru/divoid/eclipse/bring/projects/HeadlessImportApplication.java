cdfaa3b059ea4a16105924412838e6733d636777
Dmitry Konnov <konnov.diman@gmail.com>
2019-02-08 22:27:54 +0700

    Added feature and update-site.

---
+++ eclipse-import-projects-plugin/src/ru/divoid/eclipse/bring/projects/HeadlessImportApplication.java
@@ -0,0 +1,18 @@
+package ru.divoid.eclipse.bring.projects;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class HeadlessImportApplication implements IApplication {
+
+    @Override
+    public Object start(IApplicationContext iac) throws Exception {
+        new ImportProjects().earlyStartup();
+        return null;
+    }
+
+    @Override
+    public void stop() {
+    }
+
+}
