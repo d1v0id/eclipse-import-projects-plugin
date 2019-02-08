1f260dc71d4b043564af34535a17a876aaf6e09e
Dmitry Konnov <konnov.diman@gmail.com>
2019-02-09 01:01:58 +0700

    Dirty version of exclude project feature.

--- eclipse-import-projects-plugin/src/ru/divoid/eclipse/bring/projects/ImportProjects.java
+++ eclipse-import-projects-plugin/src/ru/divoid/eclipse/bring/projects/ImportProjects.java
@@ -4,7 +4,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
@@ -19,67 +27,95 @@ import org.osgi.framework.ServiceReference;
 
 public class ImportProjects implements IStartup {
 
-    private static final String IMPORT_ARGUMENT = "-import";
-    
-    private static final String SEARCHING_MESSAGE = "Searching for projects in %s";
-    private static final String REFRESHING_MESSAGE = "Refreshing project %s %s";
-    private static final String IMPORTING_PROJECT = "Importing project %s %s";
-    private static final String PROJECT_PATTERN = "\\.project";
-    
-    private final Log log = new Log();
-    
-    @Override
-    public void earlyStartup() {
-        List<String> importPaths = getImportPaths();
+	private static final String IMPORT_ARGUMENT = "i";
+	private static final String EXCLUDE_ARGUMENT = "e";
+	private static final String IMPORT_LONG_ARGUMENT = "import";
+	private static final String EXCLUDE_LONG_ARGUMENT = "exclude";
+	private static final String IMPORT_ARGUMENT_DESCRIPTION = "Paths for searching projects for import";
+	private static final String EXCLUDE_ARGUMENT_DESCRIPTION = "Project names which should be excluded";
+	private static final String VALUE_SEPARATOR = " ";
+	private static final String SEARCHING_MESSAGE = "Searching for projects in %s";
+	private static final String REFRESHING_MESSAGE = "Refreshing project %s %s";
+	private static final String IMPORTING_PROJECT = "Importing project %s %s";
+	private static final String PROJECT_PATTERN = "\\.project";
 
-        for (String path : importPaths) {
-        	log.info(String.format(SEARCHING_MESSAGE, path));
-            List<File> projectFiles = findFilesRecursively(path, PROJECT_PATTERN, new ArrayList<>());
-            projectFiles.forEach(this::importProject);
-        }
-    }
+	private final Log log = new Log();
 
-	private List<String> getImportPaths() {
-        BundleContext context = Activator.getContext();
-        ServiceReference<?> serviceReference = context.getServiceReference(IApplicationContext.class.getName());
-        IApplicationContext applicationContext = (IApplicationContext) context.getService(serviceReference);
-        String[] args = (String[]) applicationContext.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-        List<String> importPaths = new ArrayList<>();
+	private List<String> importPaths = new ArrayList<>();
+	private List<String> excludeProjects = new ArrayList<>();
 
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
-            if (arg.compareToIgnoreCase(IMPORT_ARGUMENT) == 0) {
-                i++;
-                if (i < args.length) {
-                    importPaths.add(args[i]);
-                }
-            }
-        }
+	@Override
+	public void earlyStartup() {
+		parseArguments();
 
-        return importPaths;
-    }
+		for (String path : importPaths) {
+			log.info(String.format(SEARCHING_MESSAGE, path));
+			List<File> projectFiles = findFilesRecursively(path, new ArrayList<>());
+			projectFiles.forEach(this::importProject);
+		}
+	}
 
-    private List<File> findFilesRecursively(String path, String pattern, List<File> returnedList) {
-        File root = new File(path);
-        File[] list = root.listFiles();
+	private void parseArguments() {
+		BundleContext context = Activator.getContext();
+		ServiceReference<?> serviceReference = context.getServiceReference(IApplicationContext.class.getName());
+		IApplicationContext applicationContext = (IApplicationContext) context.getService(serviceReference);
+		String[] args = (String[]) applicationContext.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+		
+		Option importOption = createArgument(IMPORT_ARGUMENT, IMPORT_LONG_ARGUMENT, IMPORT_ARGUMENT_DESCRIPTION, true);
+		Option excludeOption = createArgument(EXCLUDE_ARGUMENT, EXCLUDE_LONG_ARGUMENT, EXCLUDE_ARGUMENT_DESCRIPTION,
+				false);
 
-        if (list == null) {
-            return returnedList;
-        }
+		Options options = new Options();
+		options.addOption(importOption);
+		options.addOption(excludeOption);
+		CommandLineParser parser = new DefaultParser();
 
-        for (File file : list) {
-            if (file.isDirectory()) {
-                findFilesRecursively(file.getAbsolutePath(), pattern, returnedList);
-            } else if (Pattern.matches(pattern, file.getName())) {
-            	returnedList.add(file);
-            }
-        }
+		try {
+			CommandLine commandLine = parser.parse(options, args);
+			importPaths = parseArgumentValue(commandLine, IMPORT_ARGUMENT);
+			excludeProjects = parseArgumentValue(commandLine, EXCLUDE_ARGUMENT);
+		} catch (ParseException e) {
+			log.error(e);
+		}
+	}
+	
+	private Option createArgument(String name, String longName, String description, boolean required) {
+		return Option.builder(name).required(required).desc(description).longOpt(longName).build();
+	}
+	
+	private List<String> parseArgumentValue(CommandLine commandLine, String argument) {
+		List<String> values = new ArrayList<>();
 
-        return returnedList;
-    }
+		if (commandLine.hasOption(argument)) {
+			String importPathsValue = commandLine.getOptionValue(argument);
+			values = Stream.of(importPathsValue.split(VALUE_SEPARATOR)).collect(Collectors.toList());
+		}
 
-    private void importProject(File projectFile) {
-    	try {
+		return values;
+	}
+
+	private List<File> findFilesRecursively(String path, List<File> returnedList) {
+		File root = new File(path);
+		File[] list = root.listFiles();
+
+		if (list == null) {
+			return returnedList;
+		}
+
+		for (File file : list) {
+			if (file.isDirectory()) {
+				findFilesRecursively(file.getAbsolutePath(), returnedList);
+			} else if (Pattern.matches(PROJECT_PATTERN, file.getName()) &&
+					!excludeProjects.contains(file.getParent())) {
+				returnedList.add(file);
+			}
+		}
+
+		return returnedList;
+	}
+
+	private void importProject(File projectFile) {
+		try {
 			IWorkspace workspace = ResourcesPlugin.getWorkspace();
 			Path projectFilePath = new Path(projectFile.toString());
 			IProjectDescription description = workspace.loadProjectDescription(projectFilePath);
@@ -96,5 +132,5 @@ public class ImportProjects implements IStartup {
 		} catch (CoreException e) {
 			log.error(e);
 		}
-    }
+	}
 }
